f52cf891c7c2e84859584df48d0749b09eaaa1b6
RenggoPandora <161902414+RenggoPandora@users.noreply.github.com>
2024-03-02 20:07:48 +0700

    Add Unit 3 files
    Renggo Harya Pandora H1D023012

---
+++ Unit 3/latihan.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int elemen, i, total = 0;
+
+int main () {
+    printf ("Masukkan jumlah elemen yang ingin dimasukkan dalam array = ");
+    scanf ("%d", &elemen);
+
+    int array[elemen];
+
+    printf ("Masukkan nilai tiap elemen : \n");
+    for (i=0;i<elemen;i++) {
+        printf ("Elemen ke-%d= ", i + 1);
+        scanf ("%d", &array[i]);
+    }
+
+    for (i=0;i < elemen;i++) {
+        total += array[i];
+    }
+
+    printf ("Hasil penjumlahan seluruh elemen pada array = %d", total);
+    return 0;
+}
+
